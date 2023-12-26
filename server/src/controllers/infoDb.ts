3a98324d7fbed6974143ca347692231fe4eee65c
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-12-26 16:00:33 -0300

    infodb change

--- server/src/controllers/infoDb.ts
+++ server/src/controllers/infoDb.ts
@@ -2,24 +2,25 @@ import { findMany } from "../prismaFunctions/prisma";
 import { Request, Response } from "express";
 
 const infoDb = async (req: Request, res: Response) => {
-    const { admin } = req.params as { admin: string | null };
+  const { admin } = req.params as { admin: string | null };
 
-    try {
-        const products = await findMany('product');
-        let users = await findMany('user', {
-            adress: true,
-            cart: true,
-        });
-        if (admin !== 'true') { users = null }
-        const testimonials = await findMany('testimonial');
-        const partners = await findMany('partner', {
-            cart: true,
-        });
-
-        res.json({ products, users, testimonials, partners })
-    } catch (error: any) {
-
+  try {
+    const products = await findMany("product");
+    let users = await findMany("user", {
+      cart: true,
+    });
+    if (admin !== "true") {
+      users = null;
     }
-}
+    const testimonials = await findMany("testimonial");
+    const partners = await findMany("partner", {
+      cart: true,
+    });
 
-export default infoDb
+    res.json({ products, users, testimonials, partners });
+  } catch (error: any) {
+    console.log(error);
+  }
+};
+
+export default infoDb;
