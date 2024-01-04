bad2d1f532a978285ec0d6e868b77f1092af4f34
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2024-01-04 20:26:36 -0300

    sem msg

--- push.sh
+++ push.sh
@@ -8,6 +8,9 @@ branch_name=""
 while [ -z "$branch_name" ]; do
   echo "Enter the branch name (this field is mandatory):"
   read branch_name
+  if [ -z "$branch_name" ]; then
+    echo "Error: You must enter a branch name."
+  fi
 done
 
 cd "$directory" && git add . && git commit -m "$commit_message" && git push origin "$branch_name"
