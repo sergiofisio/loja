15cee8fad03f73115faffff7504c2032c3f29002
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2024-01-04 11:55:27 -0300

    iframe

--- user/src/pages/cart/index.jsx
+++ user/src/pages/cart/index.jsx
@@ -755,6 +755,7 @@ export default function Cart() {
               className="z-50 w-full h-[95%]"
               src={urlCheckout}
               title="Payment"
+              sandbox="allow-scripts allow-same-origin"
             ></iframe>
           </div>
         </div>
