2d6f7edd713609e0c396aead6e3e2d1f68d504d4
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2024-01-11 19:16:51 -0300

    lazy

--- user/src/pages/home/index.jsx
+++ user/src/pages/home/index.jsx
@@ -67,77 +67,97 @@ export default function Home({ login, setLogin, singIn, setSingIn }) {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <Suspense fallback={<div>Loading...</div>}>
-        <section className="flex w-full min-h-full">
-          <div className="flex items-center justify-center w-full h-[40rem] px-32">
-            {!login && !singIn ? (
-              <div className="relative flex flex-col gap-10">
-                <div className="absolute top-[3%] -left-[5%]">
-                  <img
-                    className="absolute -top-[10%]"
-                    src={logo}
-                    alt="img logo"
-                  />
-                  <img
-                    className="absolute top-[3%] -left-[5%]"
-                    src={leaftMini}
-                    alt="img leaf-mini"
-                  />
-                </div>
-                <div className="flex flex-col gap-5">
-                  <h1 className="text-black font-main text-5xl font-bold">
-                    Traga equilíbrio para sua vida com soluções naturais!
-                  </h1>
-                  <h2 className="text-black text-3xl font-light">
-                    Promovendo saúde e vitalidade com uma abordagem holística
-                    utilizando remédios naturais.
-                  </h2>
-                </div>
-                <div className="flex items-center gap-7">
+      <section className="flex w-full min-h-full">
+        <div className="flex items-center justify-center w-full h-[40rem] px-32">
+          {!login && !singIn ? (
+            <div className="relative flex flex-col gap-10">
+              <div className="absolute top-[3%] -left-[5%]">
+                <img
+                  className="absolute -top-[10%]"
+                  src={logo}
+                  alt="img logo"
+                />
+                <img
+                  className="absolute top-[3%] -left-[5%]"
+                  src={leaftMini}
+                  alt="img leaf-mini"
+                />
+              </div>
+              <div className="flex flex-col gap-5">
+                <h1 className="text-black font-main text-5xl font-bold">
+                  Traga equilíbrio para sua vida com soluções naturais!
+                </h1>
+                <h2 className="text-black text-3xl font-light">
+                  Promovendo saúde e vitalidade com uma abordagem holística
+                  utilizando remédios naturais.
+                </h2>
+              </div>
+              <div className="flex items-center gap-7">
+                <Suspense
+                  fallback={
+                    <div className="flex flex-col justify-center items-center w-full h-full">
+                      <div>
+                        <PulseLoader color="#000" />
+                        <h2>Carregando...</h2>
+                      </div>
+                    </div>
+                  }
+                >
                   <Button
                     onClick={handleBtnClick}
                     type="submit"
                     className="bg-black h-20 text-3xl rounded-r-2xl rounded-bl-3xl"
                     text="Entrar"
                   />
-                  <h2>10% OFF para novos usuários</h2>
-                </div>
+                </Suspense>
+                <h2>10% OFF para novos usuários</h2>
               </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center w-full h-full">
-                <Form
-                  login={login}
-                  setLogin={setLogin}
-                  singIn={singIn}
-                  setSingIn={setSingIn}
-                />
-                <h2 className="w-full text-center font-special">
-                  Esqueceu sua senha?
-                  <p className="cursor-pointer hover:drop-shadow-md">
-                    Clique aqui
-                  </p>
-                </h2>
-              </div>
-            )}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center w-full h-full">
+              <Form
+                login={login}
+                setLogin={setLogin}
+                singIn={singIn}
+                setSingIn={setSingIn}
+              />
+              <h2 className="w-full text-center font-special">
+                Esqueceu sua senha?
+                <p className="cursor-pointer hover:drop-shadow-md">
+                  Clique aqui
+                </p>
+              </h2>
+            </div>
+          )}
+        </div>
+        <div className="right relative flex w-full h-[50rem]">
+          <img
+            className="home-products absolute min-h-[50%] max-h-[63%] bottom-[11%] left-[3%]"
+            src={remedy}
+            alt="imgProducts"
+          />
+          <img
+            className="absolute h-[83%] b-[4%] right-0"
+            src={leaft}
+            alt="imgLeaf"
+          />
+          <img
+            className="absolute h-[37%] top-0 right-[16%]"
+            src={leaftArrow}
+            alt="imgLeaf"
+          />
+        </div>
+      </section>
+      <Suspense
+        fallback={
+          <div className="flex flex-col justify-center items-center w-full h-full">
+            <div>
+              <PulseLoader color="#000" />
+              <h2>Carregando...</h2>
+            </div>
           </div>
-          <div className="right relative flex w-full h-[50rem]">
-            <img
-              className="home-products absolute min-h-[50%] max-h-[63%] bottom-[11%] left-[3%]"
-              src={remedy}
-              alt="imgProducts"
-            />
-            <img
-              className="absolute h-[83%] b-[4%] right-0"
-              src={leaft}
-              alt="imgLeaf"
-            />
-            <img
-              className="absolute h-[37%] top-0 right-[16%]"
-              src={leaftArrow}
-              alt="imgLeaf"
-            />
-          </div>
-        </section>
+        }
+      >
         {init ? (
           <>
             <Sellers products={infoDb.produtos} />
