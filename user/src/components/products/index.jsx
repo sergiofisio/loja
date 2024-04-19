6c63169e9d9bc2a02060c4d2b0a04ba916cbdd13
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-04-19 08:50:19 -0300

    store

--- user/src/components/products/index.jsx
+++ user/src/components/products/index.jsx
@@ -1,28 +1,26 @@
 export default function Seller({ onClick, img, name, priceFull, stock }) {
   return (
     <div
-      className={`flex flex-col justify-around items-center w-72 h-full border-2 border-solid rounded-3xl p-4 relative ${
+      className={`flex flex-col justify-center items-center w-72 h-[20rem] border-2 border-solid rounded-3xl p-4 relative ${
         !stock
           ? "bg-gray-400 bg-opacity-30 border-gray-400 border-opacity-30"
           : "border-green"
-      }`}
+      } md:h-96`}
     >
-      <div className="flex flex-col items-center justify-between h-4/5 md:h-full">
+      <div className="flex flex-col items-center justify-between w-full h-full">
         <img
-          className={`${!stock ? "grayscale-[100%]" : ""} h-1/2 md:h-1/3`}
+          className={`${!stock ? "grayscale-[100%]" : ""} max-h-[70%]`}
           src={img}
           alt={`img ${name}`}
         />
-        <h1
-          className={` font-main text-xl font-semibold ${
-            !stock ? "line-through" : "text-green"
-          }`}
-        >
-          {name}
-        </h1>
-      </div>
-      <div className="flex flex-col gap-8">
-        <div className="flex justify-center font-semibold items-center gap-3">
+        <div className="flex flex-col items-center justify-center">
+          <h1
+            className={`text-center font-main text-xl font-semibold ${
+              !stock ? "line-through" : "text-green"
+            }`}
+          >
+            {name}
+          </h1>
           <h3
             className={`text-green font-main text-2xl ${
               !stock ? "font-bold" : ""
