7b16a642ec7fc5a6a40666246ceeef4b6ea7c131
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-07-02 15:09:10 -0300

    delete files

--- server/src/route/openRoute.js
+++ server/src/route/openRoute.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const schedule = require("node-schedule");
 const serveFavicon = require("serve-favicon");
+
 const productInfo = require("../controllers/product/product.info");
 const newToken = require("../controllers/user/user.token");
 const register = require("../controllers/user/user.register");
@@ -11,24 +12,18 @@ const infoHome = require("../controllers/infoHome");
 const getCategories = require("../controllers/categories");
 const infoDb = require("../controllers/infoDb");
 const userPassword = require("../controllers/user/user.password");
+const allUsersInfo = require("../controllers/user/user.allUsers");
+const backup = require("../controllers/backup");
 
 const changePassword = userPassword.changePassword;
 const recoverPassword = userPassword.recoverPassword;
 
 const express = require("express");
-const allUsersInfo = require("../controllers/user/user.allUsers");
-const backup = require("../controllers/backup");
 const openRoute = express.Router();
-
-openRoute.get("/", (_, res) => {
-  res.json({ init: true });
-});
-openRoute.get("", (_, res) => {
-  res.json({ init: true });
-});
 
 openRoute.use(serveFavicon(path.join(__dirname, "..", "..", "favicon.ico")));
 
+openRoute.get(["/", ""], (_, res) => res.json({ init: true }));
 openRoute.get("/allUsersInfo", allUsersInfo);
 openRoute.get("/infoDb/:admin", infoDb);
 openRoute.get("/productInfo/:productId", productInfo);
@@ -43,8 +38,8 @@ openRoute.post("/verify", verify);
 openRoute.post("/resetPassword", recoverPassword);
 openRoute.put("/changePassword/:token", changePassword);
 
-module.exports = openRoute;
-
 schedule.scheduleJob("0 11 * * *", function () {
   backup();
 });
+
+module.exports = openRoute;
