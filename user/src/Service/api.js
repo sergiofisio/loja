6163c8fe586f8019728d1a8fd60cd27505455969
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-05-14 13:04:37 -0300

    teste erro contexto

--- user/src/Service/api.js
+++ user/src/Service/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("URL_DEPLOY", import.meta.env.VITE_URL_DEPLOY);
+
 export default axios.create({
   baseURL: import.meta.env.VITE_URL_DEPLOY,
   timeout: 200000,
